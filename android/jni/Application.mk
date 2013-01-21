2db1d8e98493643bc8dd43e4c1ad8b150e400f7f
Ngoc Dao <ngocdaothanh@gmail.com>
2013-01-21 16:26:36 +0900

    Support Android 2.2

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,6 +1,6 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
 APP_ABI := armeabi armeabi-v7a
-APP_PLATFORM := android-10
+APP_PLATFORM := android-8
 
 APP_STL := stlport_static
 
